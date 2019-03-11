ea4b48972206abe16cd85c5f39c661cea05b2a08
adamnapieralski <adam.napieralski@gmail.com>
2019-03-12 00:34:10 +0100

    Working camera calibration

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ imgPoints = []
 
 chessboardPattern = (6, 6)
 
-testImgPath = glob.glob("./cc-test-images/*")
+testImgPath = glob.glob("./cc-test-images-2/*")
 
 cv2.namedWindow("Camera calibration", cv2.WINDOW_AUTOSIZE)
 
@@ -17,17 +17,24 @@ objPt[:, :2] = np.mgrid[0:chessboardPattern[0], 0:chessboardPattern[1]].T.reshap
 for image in testImgPath:
     calibImg = cv2.imread(image)
     imH, imW = calibImg.shape[:2]
-    imgReSize = (int(imW / 8), int(imH / 8))
+    imgReSize = (int(imW), int(imH))
     calibImg = cv2.resize(calibImg, imgReSize)
     ret, corners = cv2.findChessboardCorners(calibImg, chessboardPattern)
     cv2.drawChessboardCorners(calibImg, chessboardPattern, corners, ret)
     if ret:
         imgPoints.append(corners)
         objPoints.append(objPt)
-    cv2.imshow("Camera calibration", calibImg)
-    cv2.waitKey(200)
+        cv2.imshow("Camera calibration", calibImg)
+    cv2.waitKey(50)
 
 retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(objPoints, imgPoints, imgReSize, None, None)
+
+optCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imgReSize, 1)
+print(cameraMatrix)
+print(optCameraMatrix)
+print(roi)
+
+x, y, w, h = roi
 
 if retval:
     np.savetxt('cameraMatrix.csv', cameraMatrix)
@@ -38,5 +45,25 @@ dstCoeffs = np.genfromtxt('distCoeffs.csv')
 
 print(camMat)
 print(dstCoeffs)
+img = cv2.imread('./pic.jpg')
+imH, imW = img.shape[:2]
+imgReSize = (int(imW), int(imH))
+optCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(camMat, dstCoeffs, imgReSize, 1)
+print(optCameraMatrix)
+print(roi)
+x, y, w, h = roi
+
+imgUndist = cv2.undistort(img, camMat, dstCoeffs, None, optCameraMatrix)
+cv2.imwrite('result.jpg', img)
+imgUndist = imgUndist[y:y+h, x:x+w]
+cv2.imwrite('resultCropped.jpg', imgUndist)
+# cv2.imshow("Camera Calibration", imgUndist)
+
+cv2.waitKey(2000)
+
+# print(camMat)
+# print(dstCoeffs)
+
+
 
 cv2.destroyAllWindows()
